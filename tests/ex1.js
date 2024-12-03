ba75e2c7f79ad9ba3e1ce542afacd8e448b5ece4
Duc <ng.qc.duc@gmail.com>
2024-12-03 20:04:36 +0700

    lession 2

---
+++ tests/ex1.js
@@ -0,0 +1,13 @@
+const number = 12;
+
+let name = "my number";
+
+let isEven = false;
+
+if(number % 2 == 0){
+    isEven = true
+}
+else isEven = false
+
+console.log(isEven)
+
---
+++ tests/ex4.js
@@ -0,0 +1,5 @@
+var weighBefore = 170;
+
+var weightAfter = weighBefore - 100;
+
+console.log(" Cân nặng lý tượng của bạn là: " + weightAfter*9/10 + "/ Cân nặng tối đa là: " + weightAfter + "/ Cân nặng tối thiểu là: "+ weightAfter*8/10);
